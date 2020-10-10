3fc817684790d75ede218e0925b53a9f8ad31848
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2020-10-11 02:13:09 +0700

    menambahkan icon pada footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,12 @@ function Footer() {
     return (
         <div class="text-center py-4 mt-2 bg-primary text-white">
             <h5>React JS API</h5>
-            <a target="blank" href="https://github.com/muhyiabdulb/react-api/" class="text-white">Muhyi Abdul Basith</a>
+            <a target="blank" href="https://github.com/muhyiabdulb/react-api/" class="text-white">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cursor" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z"/>
+                </svg>
+                 Muhyi Abdul Basith
+            </a>
         </div>
     )
 };
