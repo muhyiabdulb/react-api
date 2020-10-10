5eb71dcd569d713e25b657957219a80258cd8ce3
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2020-10-11 00:42:51 +0700

    Updates

---
+++ precache-manifest.16f9c76920a721621b45a3dd5cc485b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcef3a08cc1ee207bfe4305390130f63",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "439767adfadae31b2d75",
+    "url": "/react-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46c8d3349275911df887",
+    "url": "/react-api/static/js/2.42dd37ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-api/static/js/2.42dd37ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439767adfadae31b2d75",
+    "url": "/react-api/static/js/main.f44206d6.chunk.js"
+  },
+  {
+    "revision": "e28bff25b4e364454cab",
+    "url": "/react-api/static/js/runtime-main.f8bbf804.js"
+  }
+]);
